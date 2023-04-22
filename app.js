f46f38fda52c42ac07adc83a111d2cf868ac3189
Fernando Gonzalez <fernandogonzalezvaldz@gmail.com>
2023-04-22 13:39:49 -0400

    first push from VS  CODE

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Andrea Chacon");
+    res.send("Andrea Chacon D. ");
 });
 
 app.listen(process.env.port || 3000);
